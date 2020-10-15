2df3fd82cce5e3d3128c52b8dac9bfb19a611a4a
Erik <erik.daniel.nobleta@oocl.com>
2020-10-15 17:03:37 +0800

    [MarsRover][Erik-1]test: added test case for when given command M return x 0 y 1 N

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,25 @@
+package com.afs.tdd;
+
+public class MarsRover {
+    private int xLocation;
+    private int yLocation;
+    private String direction;
+
+    public MarsRover(int xLocation, int yLocation, String direction) {
+        this.xLocation = xLocation;
+        this.yLocation = yLocation;
+        this.direction = direction;
+    }
+
+    public int getXLocation() {
+        return 0;
+    }
+
+    public int getYLocation() {
+        return 0;
+    }
+
+    public String getDirection() {
+        return null;
+    }
+}
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -1,10 +1,21 @@
 package com.afs.tdd;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    void should_x_0_y_1_N_when_commandsExecuted_M_given_x_0_y_0_N() {
+        MarsRover marsRover = new MarsRover(0,0,"N");
 
+        int xLocation = marsRover.getXLocation();
+        int yLocation = marsRover.getYLocation();
+        String direction = marsRover.getDirection();
+
+        assertEquals(xLocation, 0);
+        assertEquals(yLocation, 1);
+        assertEquals(direction, "N");
     }
 }
