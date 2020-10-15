c1cfea0e2af6783d59c3ab74b7a39442df3b011a
Erik <erik.daniel.nobleta@oocl.com>
2020-10-15 20:07:34 +0800

    [MarsRover][Erik-1]test: add test cases when execute command M given x 0 y 0 W return x -1 y 0 W

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -140,4 +140,19 @@ class DemoTest {
         assertEquals("S", direction);
     }
 
+    @Test
+    void should_x_negative_1_y_0_W_when_executeCommand_M_given_x_0_y_0_W() {
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+        CommandSplitter commandsSplit = new CommandSplitter();
+        marsRover.executeCommand(commandsSplit.splitCommands("M"));
+
+        int xLocation = marsRover.getXLocation();
+        int yLocation = marsRover.getYLocation();
+        String direction = marsRover.getDirection();
+
+        assertEquals(-1, xLocation);
+        assertEquals(0, yLocation);
+        assertEquals("E", direction);
+    }
+
 }
